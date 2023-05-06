2aab00cf0ef5dbdae03e4c864d49b7b3c9609fc8
Ritika Lal <ritikalal911@gmail.com>
2023-05-06 21:44:39 +0530

    first commit

---
+++ music player/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace music_player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.close = new System.Windows.Forms.Button();
+            this.loginlayout = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.signup = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.fpwd = new System.Windows.Forms.LinkLabel();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.loginlayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.close);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1392, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // close
+            // 
+            this.close.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
+            this.close.BackColor = System.Drawing.Color.Black;
+            this.close.BackgroundImage = global::music_player.Properties.Resources.close2;
+            this.close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close.FlatAppearance.BorderSize = 0;
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close.ForeColor = System.Drawing.SystemColors.Control;
+            this.close.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.close.Location = new System.Drawing.Point(1371, 7);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(15, 15);
+            this.close.TabIndex = 0;
+            this.close.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // loginlayout
+            // 
+            this.loginlayout.BackColor = System.Drawing.Color.White;
+            this.loginlayout.Controls.Add(this.label4);
+            this.loginlayout.Controls.Add(this.pictureBox1);
+            this.loginlayout.Controls.Add(this.label3);
+            this.loginlayout.Controls.Add(this.signup);
+            this.loginlayout.Controls.Add(this.label5);
+            this.loginlayout.Controls.Add(this.label2);
+            this.loginlayout.Controls.Add(this.label1);
+            this.loginlayout.Controls.Add(this.login_btn);
+            this.loginlayout.Controls.Add(this.fpwd);
+            this.loginlayout.Controls.Add(this.pwd);
+            this.loginlayout.Controls.Add(this.username);
+            this.loginlayout.Location = new System.Drawing.Point(524, 206);
+            this.loginlayout.Name = "loginlayout";
+            this.loginlayout.Size = new System.Drawing.Size(358, 444);
+            this.loginlayout.TabIndex = 1;
+            this.loginlayout.Paint += new System.Windows.Forms.PaintEventHandler(this.loginlayout_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(125, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 35);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::music_player.Properties.Resources.logoicon;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 59);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Kristen ITC", 6F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.Location = new System.Drawing.Point(302, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Musica";
+            // 
+            // signup
+            // 
+            this.signup.AutoSize = true;
+            this.signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signup.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup.ForeColor = System.Drawing.Color.Blue;
+            this.signup.Location = new System.Drawing.Point(115, 380);
+            this.signup.Name = "signup";
+            this.signup.Size = new System.Drawing.Size(118, 17);
+            this.signup.TabIndex = 11;
+            this.signup.Text = "CREATE ACCOUNT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(110, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Don\'t have account?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 9F);
+            this.label2.Location = new System.Drawing.Point(79, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 9F);
+            this.label1.Location = new System.Drawing.Point(79, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.Black;
+            this.login_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.login_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.login_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.login_btn.Location = new System.Drawing.Point(103, 300);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(141, 54);
+            this.login_btn.TabIndex = 3;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = false;
+            // 
+            // fpwd
+            // 
+            this.fpwd.AutoSize = true;
+            this.fpwd.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fpwd.Location = new System.Drawing.Point(61, 261);
+            this.fpwd.Name = "fpwd";
+            this.fpwd.Size = new System.Drawing.Size(112, 17);
+            this.fpwd.TabIndex = 2;
+            this.fpwd.TabStop = true;
+            this.fpwd.Text = "Forgot Password?";
+            // 
+            // pwd
+            // 
+            this.pwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.pwd.Location = new System.Drawing.Point(64, 210);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(236, 38);
+            this.pwd.TabIndex = 1;
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.username.Location = new System.Drawing.Point(64, 134);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(236, 38);
+            this.username.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Kristen ITC", 6F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(25, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Musica";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::music_player.Properties.Resources.logoicon;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 25);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::music_player.Properties.Resources.bg__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1392, 828);
+            this.Controls.Add(this.loginlayout);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.loginlayout.ResumeLayout(false);
+            this.loginlayout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Panel loginlayout;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.LinkLabel fpwd;
+        private System.Windows.Forms.TextBox pwd;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label signup;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
